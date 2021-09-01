cfeb589a09530e08c7c4ec6160d7f5fd53d467fb
luzejia <52991866+luzejia@users.noreply.github.com>
2021-09-01 15:12:33 +0800

    modify func

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/luzejia/go-mod-test-a
 
 go 1.16
 
-require github.com/luzejia/go-mod-test-b v0.0.0-20210901032642-187463b043c4
+require github.com/luzejia/go-mod-test-b v0.0.1
--- helloworld/hellowrold.go
+++ helloworld/hellowrold.go
@@ -5,5 +5,5 @@ import "github.com/luzejia/go-mod-test-b/helloworldC"
 
 func HelloWorld() {
     fmt.Println("I am a, I call on b's helloworldC")
-    helloworldC.HelloWorldC()
+    helloworldC.HelloWorld()
 }
